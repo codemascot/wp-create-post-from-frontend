9aa19a01b16a69e2d04961226aec629984b2b28c
Khan Mohammad Rashedun-Naby <naby88@gmail.com>
2017-02-23 22:12:06 +0600

    PHPUnit Test Initiated

--- public/class-wp-create-post-from-frontend-public.php
+++ public/class-wp-create-post-from-frontend-public.php
@@ -1,5 +1,6 @@
 <?php
 
+
 /**
  * The public-facing functionality of the plugin.
  *
---
+++ tests/wp-nonce.test.php
@@ -0,0 +1,8 @@
+<?php
+use \PHPUnit\Framework\TestCase;
+
+class WP_Nonce_Test extends TestCase {
+    public function test_sample() {
+        $this->assertTrue(true);
+    }
+}
